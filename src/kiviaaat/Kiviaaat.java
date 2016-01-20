18f7567533ccd94611f08c36dad9b4ab4cfffc87
deslandes benjamin <deslanbe@IHM>
2016-01-20 15:42:44 +0100

    Ben sur git

---
+++ src/kiviaaat/Kiviaaat.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiviaaat;
+
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author deslanbe
+ */
+public class Kiviaaat extends JComponent{
+
+    private TableModel model;
+    
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+    }
+
+}
